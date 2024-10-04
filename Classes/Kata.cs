1ba0b064c75538b9fdf53aed4898dbbfcb745a9d
JoachimKvamme <kaj082024@gmail.com>
2024-10-04 13:01:30 +0200

    completed JosephusPermutation

--- Classes/Kata.cs
+++ Classes/Kata.cs
@@ -1,7 +1,11 @@
- 
- 
- 
- public static class Kata {
+
+
+
+using System.Data;
+using System.Numerics;
+using System.Xml.XPath;
+
+public static class Kata {
  
  public static long QueueTime(int[] customers, int n)
     {
@@ -66,4 +70,58 @@
         }
        
     }
+
+    public static string sumStrings(string a, string b)
+    {
+        BigInteger value1;
+        BigInteger value2;
+
+        if (a != "") {
+            value1 = BigInteger.Parse(a);
+        } else {
+            value1 = 0;
+        }
+        if (b != "") {
+            value2 = BigInteger.Parse(b);
+        } else {
+            value2 = 0;
+        }
+
+       
+        BigInteger sum = value1 + value2;
+        return sum.ToString();
+    }
+    public static List<object> JosephusPermutation(List<object> items, int k)
+   {
+        List<object> resultingList = new List<object>();
+        
+        int counter = 0;
+        int removeObjectIndex = -1;
+        int startIndex = 0;
+
+        while (items.Count() > 0) {
+            
+            for (int i = startIndex; i < k + startIndex; i++)
+            {
+                if (removeObjectIndex < items.Count() - 1) {
+                    removeObjectIndex += 1;
+                } else {
+                    removeObjectIndex = 0;
+                }
+                
+            }
+            resultingList.Add(items[removeObjectIndex]);
+            items.RemoveAt(removeObjectIndex);
+            counter += 1;
+            removeObjectIndex -= 1;
+            
+        }
+        foreach (var item in resultingList)
+        {
+            Console.WriteLine(item);
+        }
+       return resultingList;
+   }
 }
+
+
